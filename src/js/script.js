52513ad69c4cc2ead0a3853f91f7ec05e6c8f419
Czerwiec <czerrwiec@gmail.com>
2022-12-13 18:18:47 +0100

    Added dom actions, connected dom elements and setup listeners

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,52 @@
+import { mapListToDOMElements } from './dominteractions.js';
+
 const mainBtn = document.querySelector('.main-btn');
 const dropdownMenu = document.querySelector('.dropdown-menu');
 
-
 mainBtn.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('transform-class')
-} )
+	dropdownMenu.classList.toggle('transform-class');
+});
+
+class TvMaze {
+	constructor() {
+		this.viewElems = {};
+		this.showNameButtons = {};
+		this.selectedName = 'harry';
+		this.initializeApp();
+	}
+
+	initializeApp = () => {
+		this.connectDOMElements();
+		this.setupListeners();
+	};
+
+	connectDOMElements = () => {
+		const listOfIds = Array.from(document.querySelectorAll('[id]')).map(
+			(elem) => elem.id
+		);
+		const listOfGenre = Array.from(
+			document.querySelectorAll('[data-genre]')
+		).map((elem) => elem.dataset.genre);
+
+		this.viewElems = mapListToDOMElements(listOfIds, 'id');
+		this.showNameButtons = mapListToDOMElements(listOfGenre, 'data-genre');
+		console.log(this.viewElems);
+		console.log(this.showNameButtons);
+	};
+
+	setupListeners = () => {
+		Object.keys(this.showNameButtons).forEach((genre) =>
+			this.showNameButtons[genre].addEventListener(
+				'click',
+				this.setCurrentFilterByName
+			)
+		);
+	};
+
+	setCurrentFilterByName = (e) => {
+		this.selectedName = e.target.dataset.genre
+	}
+
+}
+
+document.addEventListener('DOMContentLoaded', new TvMaze());
